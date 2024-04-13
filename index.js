22c1e4fe17ea4d4ed363f7180bb5e5ee285aec48
meerajagota <meerajagota16@gmail.com>
2024-04-13 15:20:49 -0700

    Location

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function initMap() {
-    // Create a new map object centered at a specific location
+
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 40.7128, lng: -74.0060 }, // New York City coordinates
+        center: { lat: 33.6405, lng: -117.8389 }, // New York City coordinates
         zoom: 12 // Zoom level
     });
 }
