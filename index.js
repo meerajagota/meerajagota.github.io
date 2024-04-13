e8c108d734800edefc84f6cc664a1b5b25eb075c
meerajagota <meerajagota16@gmail.com>
2024-04-13 15:08:42 -0700

    W

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 function initMap() {
-    var centerPoint = { lat: 40.7128, lng: -74.0060 }; // New York City coordinates
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: centerPoint,
-        zoom: 12
-    });
-
-    var circle = new google.maps.Circle({
-        strokeColor: '#0000FF',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#0000FF',
-        fillOpacity: 0.35,
-        map: map,
-        center: centerPoint,
-        radius: 1000 // Radius in meters (1000 meters = 1 kilometer)
-    });
+    map = new google.maps.Map(document.getElementById('map'));
 }
-document.addEventListener('DOMContentLoaded', initMap);
+
