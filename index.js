80be5fe15d62f171a12fae0890df76d4d4aff22f
meerajagota <meerajagota16@gmail.com>
2024-04-13 20:55:24 -0700

    edit

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 function initMap() {
 
     /* Data points defined as an array of LatLng objects */
-   
-// Define locations with names
-  
-  // Array to hold LatLng objects
-  var heatmapData = [new google.maps.LatLng(33.6424, -117.8417),
-    new google.maps.LatLng(33.6424, -117.8417),
-    new google.maps.LatLng(33.6424, -117.8417),
-    new google.maps.LatLng(33.6424, -117.8417),
-    new google.maps.LatLng(33.6424, -117.8417)];
-  
+var heatmapData = [
+    new google.maps.LatLng(37.782, -122.447),
+    new google.maps.LatLng(37.782, -122.445),
+    new google.maps.LatLng(37.782, -122.443),
+    new google.maps.LatLng(45, -50),
+    new google.maps.LatLng(37.782, -122.439),
+    new google.maps.LatLng(37.782, -122.437),
+    new google.maps.LatLng(37.782, -122.435),
+    new google.maps.LatLng(37.785, -122.447),
+    new google.maps.LatLng(37.785, -122.445),
+    new google.maps.LatLng(37.785, -122.443),
+    new google.maps.LatLng(37.785, -122.441),
+    new google.maps.LatLng(37.785, -122.439),
+    new google.maps.LatLng(37.785, -122.437),
+    new google.maps.LatLng(37.785, -122.435)
+  ];
   
   var irvine = new google.maps.LatLng(33.6424, -117.8417);
   
@@ -23,7 +29,7 @@ function initMap() {
   var heatmap = new google.maps.visualization.HeatmapLayer({
     data: heatmapData,
     radius: 70,
-    opacity: 0.5 // Adjust opacity for visibility
+    opacity: 0.25 // Adjust opacity for visibility
 
   });
   heatmap.setMap(map);
