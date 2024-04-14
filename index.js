8b487751ccea4deee9ade81378b1660bd956b9ea
meerajagota <meerajagota16@gmail.com>
2024-04-13 17:53:11 -0700

    W

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function initMap() {
 
     var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 33.6405, lng: -117.8389 }, 
-        zoom: 18 // Zoom level
+        zoom: 12 // Zoom level
     });
 
     var aqi_data = [
