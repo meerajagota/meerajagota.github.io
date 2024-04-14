39c2d4b1affb16711b8e60f0caf574bb6ae8c132
meerajagota <meerajagota16@gmail.com>
2024-04-13 20:52:53 -0700

    W

--- index.js
+++ index.js
@@ -3,33 +3,13 @@ function initMap() {
     /* Data points defined as an array of LatLng objects */
    
 // Define locations with names
-var locations = [
-    "San Francisco, CA",
-    "New York, NY",
-    "Los Angeles, CA",
-    "London, UK",
-    "Tokyo, Japan"
-  ];
-  
-  // Initialize geocoder
-  var geocoder = new google.maps.Geocoder();
   
   // Array to hold LatLng objects
-  var heatmapData = [];
-  
-  // Loop through locations and geocode each one
-  locations.forEach(function(location) {
-    geocoder.geocode({ address: location }, function(results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        // Extract latitude and longitude from geocoding result
-        var latLng = results[0].geometry.location;
-        // Push LatLng object to heatmapData array
-        heatmapData.push(latLng);
-      } else {
-        console.log("Geocoding failed for: " + location);
-      }
-    });
-  });
+  var heatmapData = [new google.maps.LatLng(33.6424, -117.8417),
+    new google.maps.LatLng(33.6424, -117.8417),
+    new google.maps.LatLng(33.6424, -117.8417),
+    new google.maps.LatLng(33.6424, -117.8417),
+    new google.maps.LatLng(33.6424, -117.8417)];
   
   
   var irvine = new google.maps.LatLng(33.6424, -117.8417);
