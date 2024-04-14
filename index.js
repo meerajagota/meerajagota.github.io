0e76a7b989825a5057ce125438ac851853d7de93
meerajagota <meerajagota16@gmail.com>
2024-04-13 20:27:13 -0700

    CSV

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 function initMap() {
 
     /* Data points defined as an array of LatLng objects */
-var heatmapData = [
-    new google.maps.LatLng(37.782, -122.447),
-    new google.maps.LatLng(37.782, -122.445),
-    new google.maps.LatLng(37.782, -122.443),
-    new google.maps.LatLng(45, -50),
-    new google.maps.LatLng(-50, 50),
-    new google.maps.LatLng(37.782, -122.437),
-    new google.maps.LatLng(37.782, -122.435),
-    new google.maps.LatLng(37.785, -122.447),
-    new google.maps.LatLng(37.785, -122.445),
-    new google.maps.LatLng(37.785, -122.443),
-    new google.maps.LatLng(37.785, -122.441),
-    new google.maps.LatLng(37.785, -122.439),
-    new google.maps.LatLng(37.785, -122.437),
-    new google.maps.LatLng(37.785, -122.435)
-  ];
+    const lines = csvData.split('\n');
+    const headers = lines[0].split(',');
+    const heatmapData = [];
+    
+    // Find the index of relevant columns
+    const latIndex = headers.indexOf('Latitude');
+    const lngIndex = headers.indexOf('Longitude');
+    const intensityIndex = headers.indexOf('O3 Mean');
+    
+    // Extract latitude, longitude, and intensity values from each row
+    for (let i = 1; i < lines.length; i++) {
+        const values = lines[i].split(',');
+        const latitude = parseFloat(values[latIndex]);
+        const longitude = parseFloat(values[lngIndex]);
+        const intensity = parseFloat(values[intensityIndex]);
+        heatmapData.push({ location: { lat: latitude, lng: longitude }, weight: intensity });
+    }
+
+
+// var heatmapData = [
+//     new google.maps.LatLng(37.782, -122.447),
+//     new google.maps.LatLng(37.782, -122.445),
+//     new google.maps.LatLng(37.782, -122.443),
+//     new google.maps.LatLng(45, -50),
+//     new google.maps.LatLng(-50, 50),
+//     new google.maps.LatLng(37.782, -122.437),
+//     new google.maps.LatLng(37.782, -122.435),
+//     new google.maps.LatLng(37.785, -122.447),
+//     new google.maps.LatLng(37.785, -122.445),
+//     new google.maps.LatLng(37.785, -122.443),
+//     new google.maps.LatLng(37.785, -122.441),
+//     new google.maps.LatLng(37.785, -122.439),
+//     new google.maps.LatLng(37.785, -122.437),
+//     new google.maps.LatLng(37.785, -122.435)
+//   ];
   
   var irvine = new google.maps.LatLng(33.6424, -117.8417);
   
@@ -29,7 +48,7 @@ var heatmapData = [
   var heatmap = new google.maps.visualization.HeatmapLayer({
     data: heatmapData,
     radius: 70,
-    opacity: 0.25 // Adjust opacity for visibility
+    opacity: 0.5 // Adjust opacity for visibility
 
   });
   heatmap.setMap(map);
